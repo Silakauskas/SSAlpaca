f608530dc70318b62374ef71586e7de74a691836
DESKTOP-706MNH8\SS <simux9@gmail.com>
2022-04-11 19:17:24 +0300

    Hello world! Blank constants file - change your keys!

---
+++ constants.py
@@ -0,0 +1,4 @@
+# API Info for fetching data, portfolio, etc. from Alpaca
+BASE_URL = "https://paper-api.alpaca.markets"
+ALPACA_API_KEY = "<YOUR ALPACA API KEY>"
+ALPACA_SECRET_KEY = "<YOUR ALPACA SECRET KEY>"
